362612c05c439031b44625fe87d0718a63e7d4ee
Adrian <adrian.asencio92@hotmail.com>
2022-02-04 15:43:00 +0100

    probando cargar antes html que css

--- components/layout.js
+++ components/layout.js
@@ -23,13 +23,7 @@ export default function Layout({ children }) {
   return (
     <div className={styles.container}>
       <Head>
-        {/*MARKETING*/}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-218480164-1"
-        ></script>
         <title>RCS | Reformas, Construcciones y Servicios</title>
-        <link rel="icon" href="/logo-imagenes.png" />
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
@@ -37,10 +31,16 @@ export default function Layout({ children }) {
           integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
           crossOrigin="anonymous"
         ></link>
+        <link rel="icon" href="/logo-imagenes.png" />
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
           crossOrigin="anonymous"
+        ></script>
+        {/*MARKETING*/}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-218480164-1"
         ></script>
       </Head>
       <body className={styles.body}>
